3ec558a4a67de25b5f5183de1d3e9823dd12a115
Martin Hladil <martin@hladil.name>
2010-06-03 14:45:59 +0200

    csv export

--- log_csv.rb
+++ log_csv.rb
@@ -1,11 +1,13 @@
 #!/usr/bin/env ruby
-# require "rubygems"
-# gem "log_parser", "= 0.2.0"
-$:.unshift File.dirname($0) + "/../log_parser/lib"
+require "rubygems"
+gem "log_parser", "= 0.2.0"
+# $:.unshift File.dirname($0) + "/../log_parser/lib"
 require "log_parser"
 require "optparse"
+require "csv"
 
 SD_LOGS_PATH = File.dirname(File.expand_path(__FILE__)) + "/sd_logs.yml"
+CSV_PATH = File.dirname(File.expand_path(__FILE__)) + "/csv.yml"
 
 options = {}
 OptionParser.new do |opts|
@@ -22,27 +24,36 @@ end.parse!
 
 path = ARGV.first
 raise if path.nil?
-@sdlog_parser = LogParser::SDLogParser.new(SD_LOGS_PATH)
 
 def hex_log_to_binary_log(hex_log)
   hex_log.to_a.pack("H*")
 end
 
+def load_csv_definitions
+  YAML::load(File.read(CSV_PATH))
+end
+
 def process_log(file_path)
   puts "Processing '#{file_path}'..."
   File.open(file_path) do |file|
-    while !file.eof? do
-      line = file.readline
-      hex_log = line.match(/[0-9a-f]{6}:([0-9a-f]{1024})/i)
-      hex_log = hex_log.captures[0] if hex_log
-      if hex_log
-        binary_log = hex_log_to_binary_log(hex_log)
-        puts @sdlog_parser.parse(binary_log).inspect
+    CSV.open(File.join(File.dirname(file_path), File.basename(file_path) + ".csv"), "w") do |writer|
+      writer << @csv_definitions["columns"]
+      while !file.eof? do
+        line = file.readline
+        hex_log = line.match(/[0-9a-f]{6}:([0-9a-f]{1024})/i)
+        hex_log = hex_log.captures[0] if hex_log
+        if hex_log
+          binary_log = hex_log_to_binary_log(hex_log)
+          log = @sdlog_parser.parse(binary_log)
+          writer << @csv_definitions["columns"].collect { |column| column == "log_name" ? log[:name] : log[:attributes][column.to_sym] }
+        end
       end
     end
   end
 end
 
+@sdlog_parser = LogParser::SDLogParser.new(SD_LOGS_PATH)
+@csv_definitions = load_csv_definitions
 Dir[File.join(path, "*.log")].each do |file_path|
   process_log file_path
 end
